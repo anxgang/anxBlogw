9022f6e0dbf868c5429d9b460e662838ce100e34
anxgang <anxgang@users.noreply.github.com>
2023-01-08 14:29:11 +0000

    deploy: anxgang/anxBlog@b0f1cbdf7f9fab1e71852a8a6f8fdfeb10dd8793

--- assets/js/custom-mermaid.js
+++ assets/js/custom-mermaid.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-  if ($(".language-mermaid").length >= 1) {
-    mermaid.initialize({
-      startOnLoad: true,
-      theme: "default", //default neutral dark forest
-    })
-    mermaid.init({noteMargin: 10}, ".language-mermaid")
-  }
-})
+// $(document).ready(function () {
+//   if ($(".language-mermaid").length >= 1) {
+//     mermaid.initialize({
+//       startOnLoad: true,
+//       theme: "default", //default neutral dark forest
+//     })
+//     mermaid.init({noteMargin: 10}, ".language-mermaid")
+//   }
+// })
